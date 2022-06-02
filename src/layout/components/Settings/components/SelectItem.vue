c14d532aa234369deb54e60e161918c4c15cceeb
阿沐 <babamu@126.com>
2022-06-02 17:34:52 +0800

    commit

---
+++ src/layout/components/Settings/components/SelectItem.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="settings-select">
+		<span> {{ title }}</span>
+		<el-select :model-value="modelValue" size="default" style="width: 100px" :disabled="disabled" @change="handleChange($event)">
+			<el-option v-for="option in options" :key="option.value" :label="option.label" :value="option.value"></el-option>
+		</el-select>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+defineProps({
+	modelValue: {
+		type: String,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	options: {
+		type: Array as PropType<any[]>,
+		required: true,
+		default: () => []
+	},
+	disabled: {
+		type: Boolean
+	}
+})
+
+const emit = defineEmits(['update:modelValue', 'change'])
+const handleChange = (val: any) => {
+	emit('update:modelValue', val)
+	emit('change')
+}
+</script>
+
+<style lang="scss" scoped>
+.settings-select {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+</style>
---
+++ src/layout/components/Settings/components/SwitchItem.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="settings-switch">
+		<span> {{ title }}</span>
+		<el-switch :model-value="modelValue" :disabled="disabled" @change="handleChange($event)"></el-switch>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	modelValue: {
+		type: Boolean,
+		required: true
+	},
+	title: {
+		type: String,
+		required: true
+	},
+	disabled: {
+		type: Boolean
+	}
+})
+
+const emit = defineEmits(['update:modelValue', 'change'])
+const handleChange = (val: any) => {
+	emit('update:modelValue', val)
+	emit('change')
+}
+</script>
+
+<style lang="scss" scoped>
+.settings-switch {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding-bottom: 15px;
+	color: #444;
+}
+</style>
